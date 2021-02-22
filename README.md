3509cdb8cde99ebb7c7b8ee44edfb99f04f74cd8
greckko <b.soman@onvio.nl>
2021-02-22 16:50:10 +0100

    update, argparser, refactor, reporfiles

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Findsecrets
 Recursively scans a local directory for common secret keywords and strings
 
-## Update Feb 2021
-Total makeover. Removed confusing modular / class oriented code. All the magic now in findsecrets.py
+## Configuration
+Edit config.yml for the scanfolder and exclusions.\
+The scanfolder in config.yml will automatically overridden by the "-f" cmdline argument so chose either
 
 ## Instructions
-Only works with python 3.x
+Only works with Python 3.x
 
 ```pip install -r requirements.txt```<br>
-```python findsecrets.py```
+```python findsecrets.py -f <folder> -v```
+```
+optional arguments:
+  -h, --help                    show this help message and exit
+  -f, --folder FOLDER           Local Folder to scan / Cloned Repo
+  -m, --mask                    Mask Secrets (Default = False)
+  -v, --verbose                 Verbose output, shows secrets in STDOUT / Console (Default = False)
+```
 
-## Configuration
-Edit config.yml for the scanfolder and exclusions
+## Reports
+Reports are automatically saved in the current folder:
+```
+report.json
+seqhubreport.json // for SEQHUB parsing
+```
 
 ## To-Do's:
-
-*   Commandline arguments
-*   JSON Report output (for SEQHUB) incl. Masking of found secrets
+*   Git commit scanning, history, multiple branches etc. same as gitleaks.
 *   Value checks based on known secret regexes
+*   XML / JSON / YAML Parser, Traverse trough Key/Values to get secret pairs
--- findsecrets.py
+++ findsecrets.py
@@ -5,102 +5,181 @@ import os
 import jellyfish
 from yaml import safe_load
 from pathlib import Path
+import argparse
+import json
 
 if sys.version_info < (3, 0):
     sys.stdout.write("Sorry, findsecrets requires Python 3.x\n")
     sys.exit(1)
 
-## Load Configs
-def yamlconfig(configyml = Path('config.yml')):
+parser = argparse.ArgumentParser('findsecrets.py', formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=40))
+parser.add_argument('-f', '--folder', help='Folder to scan', dest='folder', required=False)
+parser.add_argument('-m', '--mask', help='Mask Secret Values', dest='mask', action='store_true', default=False)
+parser.add_argument('-v', '--verbose', help='Verbose output in stdout', dest='verbose', action='store_true', default=False)
+args = parser.parse_args()
+
+
+def yamlconfig(configyml=Path('config.yml')):
     yamldict = safe_load(configyml.read_text())
     if not isinstance(yamldict, dict):
-        return {}
+        return dict()
     return yamldict
 
+
 config = yamlconfig()
-scanfolder = config['scanfolder'][0]
+
+if args.folder is not None:
+    scanfolder = args.folder
+else:
+    try:
+        scanfolder = config['scanfolder'][0]
+    except:
+        print("Findsecrets failed... Scanfolder not specified")
+        sys.exit(1)
+
 keywordsdb = 'db/positivekeywords.txt'
+foundsecrets = dict()
+
 
 def validated_secret_value(key, value):
-    excluded_value_chars_lowercase = [each_string.lower() for each_string in config['exclude']['values']]
+    excl_values_lcase = [i.lower() for i in config['exclude']['values']]
 
-    ## Checks for values
+    # Checks values for potential ENV variables / False positives
     if len(value) < config['settings'][2]['min-value-length']:
         return False
-    if any([x in value.lower() for x in excluded_value_chars_lowercase]):
+    if any([x in value for x in excl_values_lcase]):
         return False
-    if key.lower() in value.lower():
+    if key in value:
         return False
-
-    ## Check if key string somewhat compares with value string using levenhstein distance
-    ## https://jellyfish.readthedocs.io/en/latest/comparison.html#levenshtein-distance
-
-    if jellyfish.levenshtein_distance(key, value) <= config['settings'][0]['levenshtein-distance']:
+    if value.endswith('}') or value.endswith(')') or value.endswith(';'):
         return False
 
-    ## Check if special value contains alphanumeric 8-70 string with specialchars
-    if key.lower() == 'token' or key.lower() == 'password' or key.lower() == 'value':
-        regexp = re.compile(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,100}')
-        if regexp.search(value): 
+    # Check if key somewhat compares value string using levenhstein distance
+    # https://jellyfish.readthedocs.io/en/latest/comparison.html#levenshtein-distance
+
+    ldistance = config['settings'][0]['levenshtein-distance']
+    if jellyfish.levenshtein_distance(key, value) <= ldistance:
+        return False
+
+    # Extra checks within sensitive values
+    if key in config['sensitivevalues']['sensitivevalues']:
+        if 'http' or 'https' in value:
+            return False
+        regexp2 = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,100}"
+        # https://regex101.com/r/v1qv1E/1
+        if re.findall(regexp2, value, re.IGNORECASE):
             return True
         else:
             return False
     return True
 
-def spiderfolder(scanfolder):
 
-    filepaths = []
+def spiderfolder(scanfolder):
+    try:
+        len(os.listdir(scanfolder)) == 0
+    except:
+        print(f"{scanfolder} Directory not found or is empty")
+        sys.exit(1)
+
+    print(f'\nFindsecrets Running \nSpidering folder: {scanfolder}\n')
+    filepaths = list()
     for root, dirs, files in os.walk(scanfolder, topdown=True):
-        ## Exclude Dirs
+        # Exclude Dirs
         dirs[:] = [d for d in dirs if d not in config['exclude']['folders']]
-        ## Exclude Extentions
-        files = [ file for file in files if not file.endswith( tuple(config['exclude']['extentions']) ) ]
-        ## Exclude Filenames
-        files = [ file for file in files if not file.endswith( tuple(config['exclude']['files']) ) ]
-        ## Exclude Large Filesizes
+        # Exclude Extentions
+        files = [file for file in files if not file.endswith(tuple(config['exclude']['extentions']))]
+        # Exclude Filenames
+        files = [file for file in files if not file.endswith(tuple(config['exclude']['files']))]
+        # Exclude Large Filesizes
         for i in files:
             filesize = "%.0f" % (os.path.getsize(os.path.join(root, i)) / 1024)
             if int(filesize) <= int(config['settings'][1]['max-filesize-kb']):
                 filepaths.append(os.path.join(root, i))
+    print(f'{len(filepaths)} files will be scanned for secrets\n')
     return filepaths
 
+
 def find_keywords_with_values_in_file(file):
-        assignment = "(\\b|[ ._-])({})[ '\"]*(=>|=|:)[ '\"]*([^'\" ]+)"
-        keywords = open(keywordsdb).read().splitlines()
-    
-        # alternative assignment = "(\b{})(=>|:|=)(.*)"
-        assignment_pattern = assignment.format('|'.join(keywords))
-        # assignment pattern example https://regex101.com/r/DzO3jR/1
+    assignment = "(\\b|[ ._-])({})[ '\"]*(=>|=|:)[ '\"]*([^'\" ]+)"
+    keywords = open(keywordsdb).read().splitlines()
 
-        filename = str(file)
-        matchesdict = {}
-        matchesdict[filename] = []
-        try:
-            file = open(file, 'r', encoding='utf8')
-        except:
-            pass
+    # alternative assignment = "(\b{})(=>|:|=)(.*)"
+    assignment_pattern = assignment.format('|'.join(keywords))
+    # assignment pattern example https://regex101.com/r/DzO3jR/1
 
-        # Read each lines from the file & remove empty lines
-        try:
-            for line in file:
-                line = line.strip()
-                if line != '':
-            # Get all match groups within line string
-                    matches = re.findall(assignment_pattern, line, re.IGNORECASE)
-                    if matches:
-                        for i in matches:
-                            key, _, value = i[1:]
-                            if validated_secret_value(key, value):
-                                key_value = key + ':' + value
-                                matchesdict[filename].append(key_value)
-                    # print(f"file read error: {file}")
-        except:
-            pass
-        for key, value in matchesdict.items():
-            if len(value) != 0 or None:
-                print(key, value)
+    filename = str(file)
+    matchesdict = dict()
+    matchesdict[filename] = list()
+    try:
+        file = open(file, 'r', encoding='utf8')
+    except:
+        pass # File Open Error
+    # Read each lines from file & remove empty lines
+    try:
+        for line in file:
+            line = line.strip()
+            if line != '':
+                # Get all match groups within line string
+                matches = re.findall(assignment_pattern, line, re.IGNORECASE)
+                if matches:
+                    for i in matches:
+                        key, _, value = i[1:]
+                        if validated_secret_value(key.lower(), value.lower()):
+                            if args.mask:
+                                value = value[4:].rjust(len(value), "*")
+                                value = value[:-4].ljust(len(value), "*")
+                            key_value = key + ':' + value
+                            matchesdict[filename].append(key_value)
+    except:
+        pass
+    for file, secret in matchesdict.items():
+        if len(secret) != 0 or None:
+            foundsecrets.update({file: secret})
+            if args.verbose:
+                print(matchesdict)
+
+
+def jsonreport(foundsecrets):
+    jsonreport = 'report.json'
+    jsonobject = {"vulnerabilities": []}
+
+    for file, secrets in foundsecrets.items():
+        with open(jsonreport, 'w'):
+            secretsdb = {
+                "File": file,
+                "Secrets": secrets,
+                "Severity": "High"
+            }
+            jsonobject['vulnerabilities'].append(secretsdb)
+
+    with open(jsonreport, 'w') as f:
+        json.dump(jsonobject, f, indent=4)
+
+
+def seqhubreport(foundsecrets):
+    seqhubreport = 'seqhubreport.json'
+    jsonobject = {"vulnerabilities": []}
+
+    for file, secrets in foundsecrets.items():
+        if len(secrets) > 1:
+            secrets = str(''.join('%s ' % secrets for secrets in secrets))
+        else:
+            secrets = secrets[0]
+        with open(seqhubreport, 'w'):
+            secretsdb = {
+                "title": "Secret Found",
+                "description": f"{file} contains: {secrets}",
+                "severity": "High"
+            }
+            jsonobject['vulnerabilities'].append(secretsdb)
+
+    with open(seqhubreport, 'w') as f:
+        json.dump(jsonobject, f, indent=4)
+
 
 filepaths = spiderfolder(scanfolder)
-
 for scannedfile in filepaths:
     find_keywords_with_values_in_file(scannedfile)
+
+jsonreport(foundsecrets)
+seqhubreport(foundsecrets)
